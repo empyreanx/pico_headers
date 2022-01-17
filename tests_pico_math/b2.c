bf8280e68aa2cdc213145edb0aad179805c95f8b
James McLean <james@wolfspider.ca>
2022-01-17 16:17:54 -0500

    Tests for test_b2_min and test_b2_transform

--- tests_pico_math/b2.c
+++ tests_pico_math/b2.c
@@ -121,11 +121,34 @@ PU_TEST(test_b2_contains)
 
 PU_TEST(test_b2_min)
 {
+    pm_v2 verts[4];
+    verts[0] = pm_v2_make(1.0f, 2.0f);
+    verts[1] = pm_v2_make(1.0f, 2.0f + 4.0f);
+    verts[2] = pm_v2_make(1.0f + 3.0f, 2.0f + 4.0f);
+    verts[3] = pm_v2_make(1.0f + 3.0f, 4.0f);
+
+    pm_b2 res = pm_b2_min(verts, 4);
+    pm_b2 exp = pm_b2_make(1.0f, 2.0f, 3.0f, 4.0f);
+
+    PU_ASSERT(pm_b2_equal(&res, &exp));
+
     return true;
 }
 
 PU_TEST(test_b2_transform)
 {
+    pm_b2 b = pm_b2_make(0.0f, 0.0f, 1.0f, 1.0f);
+
+    pm_t2 t = pm_t2_identity();
+    pm_t2_translate(&t, pm_v2_make(-0.5, -0.5));
+    pm_t2_rotate(&t, -PM_PI / 4.0f);
+
+    pm_b2 res = pm_b2_transform(&b, &t);
+    pm_flt len = pm_sin(PM_PI / 4.0f);
+    pm_b2 exp = pm_b2_make(-len, -len, 2.0 * len, 2.0 * len);
+
+    PU_ASSERT(pm_b2_equal(&res, &exp));
+
     return true;
 }
 
