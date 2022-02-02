213117cc039a93cba54c8c6d89a7c5011e4303ac
James McLean <james@wolfspider.ca>
2022-02-02 12:44:30 -0500

    Formatting

--- examples_pico_ecs/rogue/components.c
+++ examples_pico_ecs/rogue/components.c
@@ -9,11 +9,11 @@
 //
 void register_components(game_t* game)
 {
-    ecs_register_component(game->ecs, COMP_POS,      sizeof(pos_t), NULL, NULL);
-    ecs_register_component(game->ecs, COMP_DRAWABLE, sizeof(drawable_t), NULL, NULL);
-    ecs_register_component(game->ecs, COMP_STATS,    sizeof(stats_t), NULL, NULL);
-    ecs_register_component(game->ecs, COMP_PLAYER,   sizeof(player_t), NULL, NULL);
-    ecs_register_component(game->ecs, COMP_MONSTER,  sizeof(monster_t), NULL, NULL);
+    ecs_register_component(game->ecs, COMP_POS,      sizeof(pos_t),        NULL, NULL);
+    ecs_register_component(game->ecs, COMP_DRAWABLE, sizeof(drawable_t),   NULL, NULL);
+    ecs_register_component(game->ecs, COMP_STATS,    sizeof(stats_t),      NULL, NULL);
+    ecs_register_component(game->ecs, COMP_PLAYER,   sizeof(player_t),     NULL, NULL);
+    ecs_register_component(game->ecs, COMP_MONSTER,  sizeof(monster_t),    NULL, NULL);
     ecs_register_component(game->ecs, COMP_CHEST,    sizeof(chest_comp_t), NULL, NULL);
 }
 
