41d88aea57fb283df8b8821f801c251325ed414a
James McLean <james@wolfspider.ca>
2022-04-01 19:39:35 -0400

    Integrated decoding

--- pico_b64.h
+++ pico_b64.h
@@ -7,8 +7,8 @@
     --------
     A simple Base64 encoding/decoding library. This library is a repackaged
     version of the [b64.c](https://github.com/littlstar/b64.c) library into a
-    single header format. Aside from a few formatting modifications some
-    comments and macros, the code is largely true to the original.
+    single header format. Aside from a few minor modifications the code is
+    largely true to the original code by Joseph Werle.
 */
 
 #ifndef PICO_B64_H
@@ -55,6 +55,10 @@ unsigned char* b64_decode_ex(const char* buf, size_t in_size, size_t* out_size);
 #define B64_REALLOC     PICO_B64_REALLOC
 #define B64_BUFFER_SIZE PICO_B64_BUFFER_SIZE
 
+/*=============================================================================
+ * Look-up table
+ *============================================================================*/
+
 static const char b64_table[] =
 {
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
@@ -66,6 +70,10 @@ static const char b64_table[] =
   'w', 'x', 'y', 'z', '0', '1', '2', '3',
   '4', '5', '6', '7', '8', '9', '+', '/'
 };
+
+/*=============================================================================
+ * Buffer handling
+ *============================================================================*/
 
 static void* b64_buf_init(size_t* bufc);
 static void* b64_buf_resize(void* ptr, size_t size, size_t* bufc);
@@ -93,6 +101,125 @@ void* b64_buf_resize(void* ptr, size_t size, size_t* bufc) {
     return ptr;
 }
 
+/*=============================================================================
+ * Decoding
+ *============================================================================*/
+
+unsigned char* b64_decode(const char * src, size_t size) {
+        return b64_decode_ex(src, size, NULL);
+}
+unsigned char * b64_decode_ex(const char * src, size_t in_size, size_t * out_size) {
+    int i = 0;
+    int j = 0;
+    int l = 0;
+    size_t size = 0;
+    size_t bufc = 0;
+    unsigned char * dec = NULL;
+    unsigned char buf[3];
+    unsigned char tmp[4];
+
+    // alloc
+    dec = (unsigned char*)b64_buf_init(&bufc);
+    if (NULL == dec) {
+        return NULL;
+    }
+
+    // parse until end of source
+    while (in_size--) {
+        // break if char is `=' or not base64 char
+        if ('=' == src[j]) {
+            break;
+        }
+        if (!(isalnum(src[j]) || '+' == src[j] || '/' == src[j])) {
+            break;
+        }
+
+        // read up to 4 bytes at a time into `tmp'
+        tmp[i++] = src[j++];
+
+        // if 4 bytes read then decode into `buf'
+        if (4 == i) {
+            // translate values in `tmp' from table
+            for (i = 0; i < 4; ++i) {
+                // find translation char in `b64_table'
+                for (l = 0; l < 64; ++l) {
+                    if (tmp[i] == b64_table[l]) {
+                        tmp[i] = l;
+                        break;
+                    }
+                }
+            }
+
+            // decode
+            buf[0] = (tmp[0] << 2) + ((tmp[1] & 0x30) >> 4);
+            buf[1] = ((tmp[1] & 0xf) << 4) + ((tmp[2] & 0x3c) >> 2);
+            buf[2] = ((tmp[2] & 0x3) << 6) + tmp[3];
+
+            // write decoded buffer to `dec'
+            dec = (unsigned char*)b64_buf_resize(dec, size + 3, &bufc);
+            if (dec != NULL) {
+                for (i = 0; i < 3; ++i) {
+                    dec[size++] = buf[i];
+                }
+            } else {
+                return NULL;
+            }
+
+            // reset
+            i = 0;
+        }
+    }
+
+    // remainder
+    if (i > 0) {
+        // fill `tmp' with `\0' at most 4 times
+        for (j = i; j < 4; ++j) {
+            tmp[j] = '\0';
+        }
+
+        // translate remainder
+        for (j = 0; j < 4; ++j) {
+            // find translation char in `b64_table'
+            for (l = 0; l < 64; ++l) {
+                if (tmp[j] == b64_table[l]) {
+                    tmp[j] = l;
+                    break;
+                }
+            }
+        }
+
+        // decode remainder
+        buf[0] = (tmp[0] << 2) + ((tmp[1] & 0x30) >> 4);
+        buf[1] = ((tmp[1] & 0xf) << 4) + ((tmp[2] & 0x3c) >> 2);
+        buf[2] = ((tmp[2] & 0x3) << 6) + tmp[3];
+
+        // write remainer decoded buffer to `dec'
+        dec = (unsigned char*)b64_buf_resize(dec, size + (i - 1), &bufc);
+        if (dec != NULL) {
+            for (j = 0;
+                (j < i - 1); ++j) {
+                dec[size++] = buf[j];
+            }
+        } else {
+            return NULL;
+        }
+    }
+
+    // Make sure we have enough space to add '\0' character at end.
+    dec = (unsigned char*)b64_buf_resize(dec, size + 1, &bufc);
+    if (dec != NULL) {
+        dec[size] = '\0';
+    } else {
+        return NULL;
+    }
+
+    // Return back the size of decoded string if demanded.
+    if (out_size != NULL) {
+        * out_size = size;
+    }
+
+    return dec;
+}
 
 #endif // PICO_B64_IMPLEMENTATION
 
