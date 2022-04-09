c687d4608756bf51da0e8a95e281e9f6dc2dd730
James McLean <james@wolfspider.ca>
2022-04-08 22:05:05 -0400

    Updated license

--- pico_b64.h
+++ pico_b64.h
@@ -323,7 +323,7 @@ size_t b64_decode(unsigned char* dst, const char * src, size_t len)
 /*
 The MIT License (MIT)
 
-Copyright (c) 2014 Little Star Media, Inc.
+Copyright (c) 2014 Little Star Media, Inc. (Joseph Werle)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
