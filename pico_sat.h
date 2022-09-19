073e3130d8a5727cdd4ab7ca6e690f711d5909b8
James McLean <james@wolfspider.ca>
2022-09-19 16:10:54 -0400

    Initial commit

---
+++ pico_sat.h
@@ -0,0 +1,20 @@
+/*
+    @file pico_unit.h
+    @brief Separating Axis Test (SAT) written in C99.
+
+    ----------------------------------------------------------------------------
+    Licensing information at end of header
+    ----------------------------------------------------------------------------
+*/
+
+#ifndef PICO_SAT_H
+#define PICO_SAT_H
+
+#include "pico_math.h"
+
+#endif // PICO_SAT_H
+
+#ifdef PICO_SAT_IMPLEMENTATION
+
+#endif // PICO_SAT_IMPLEMENTATION
+
