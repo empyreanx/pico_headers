20bc7e1a952be76b54afd57601e72b850bda4155
James McLean <james@wolfspider.ca>
2022-09-21 19:35:37 -0400

    Changed pm_flt -> pm_float, and polygon -> poly

--- pico_sat.h
+++ pico_sat.h
@@ -19,7 +19,7 @@
 typedef struct
 {
     pm_v2  pos;
-    pm_flt radius;
+    pm_float radius;
 } sat_circle_t;
 
 typedef struct
@@ -28,33 +28,31 @@ typedef struct
     pm_v2 vertices[PICO_SAT_MAX_POLY_VERTS];
     pm_v2 normals[PICO_SAT_MAX_POLY_VERTS - 1];
     pm_v2 edges[PICO_SAT_MAX_POLY_VERTS - 1];
-} sat_polygon_t;
-// } sat_poly_t?
-typedef sat_polygon_t sat_poly_t;
+} sat_poly_t;
 
 typedef struct
 {
-    pm_v2  normal;
-    pm_flt overlap;
+    pm_v2    normal;
+    pm_float overlap;
 } sat_response_t;
 
 //later
 //pm_b2 sat_polygon_to_aabb(const sat_polygon_t* poly);
 //pm_b2 sat_circle_to_aabb(const sat_circle_t* circle);
 
-sat_circle_t sat_make_cicle(pm_v2 pos, pm_flt radius);
+sat_circle_t sat_make_cicle(pm_v2 pos, pm_float radius);
 sat_polygon_t sat_make_polygon(int vertex_count, pm_v2 vertices[]);
 sat_polygon_t sat_aabb_to_polygon(const pm_b2* aabb);
 
-bool sat_test_polygon_polygon(const sat_poly_t* p1,
+bool sat_test_poly_poly(const sat_poly_t* p1,
                               const sat_poly_t* p2,
                               sat_response_t* response);
 
-bool sat_test_circle_polygon(const sat_circle_t* c,
+bool sat_test_circle_poly(const sat_circle_t* c,
                              const sat_poly_t* p,
                              sat_response_t* response);
 
-bool sat_test_polygon_circle(const sat_poly_t* p,
+bool sat_test_poly_circle(const sat_poly_t* p,
                              const sat_circle_t* c,
                              sat_response_t* response);
 
@@ -71,10 +69,12 @@ bool sat_is_separating_axis(const sat_poly_t* p1,
                             pm_v2 axis,
                             sat_response_t* response)
 {
+
+
     return true;
 }
 
-sat_circle_t sat_make_cicle(pm_v2 pos, pm_flt radius)
+sat_circle_t sat_make_cicle(pm_v2 pos, pm_float radius)
 {
     sat_circle_t circle;
     circle.pos = pos;
@@ -100,8 +100,7 @@ sat_polygon_t sat_make_polygon(int vertex_count, pm_v2 vertices[])
         pm_v2 v1 = vertices[i];
         pm_v2 v2 = (i < vertex_count - 1) ? vertices[i + 1] : vertices[0];
         poly.edges[i] = pm_v2_sub(v2, v1);
-        poly.normals[i] = pm_v2_perp(&poly.edges[i]);
-        pm_v2_normalize(&poly.normals[i]);
+        poly.normals[i] = pm_v2_normalize(pm_v2_perp(poly.edges[i]));
     }
 
     return poly;
