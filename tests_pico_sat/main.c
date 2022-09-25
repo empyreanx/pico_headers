27ed071470b5ee328df900c7b1f40ba66bd96208
James McLean <james@wolfspider.ca>
2022-09-25 15:51:41 -0400

    Test AABB collision and no collision

--- tests_pico_sat/main.c
+++ tests_pico_sat/main.c
@@ -11,12 +11,34 @@
 
 PU_TEST(test_collide)
 {
+    pm_b2 aabb1 = pm_b2_make(5, 5, 2, 2);
+    pm_b2 aabb2 = pm_b2_make(6, 6, 2, 2);
+
+    sat_poly_t p1 = sat_aabb_to_poly(&aabb1);
+    sat_poly_t p2 = sat_aabb_to_poly(&aabb2);
+
+    PU_ASSERT(sat_test_poly_poly(&p1, &p2, NULL));
+
+    return true;
+}
+
+PU_TEST(test_not_collide)
+{
+    pm_b2 aabb1 = pm_b2_make(5, 5, 2, 2);
+    pm_b2 aabb2 = pm_b2_make(9, 9, 2, 2);
+
+    sat_poly_t p1 = sat_aabb_to_poly(&aabb1);
+    sat_poly_t p2 = sat_aabb_to_poly(&aabb2);
+
+    PU_ASSERT(!sat_test_poly_poly(&p1, &p2, NULL));
+
     return true;
 }
 
 static PU_SUITE(suite_sat)
 {
     PU_RUN_TEST(test_collide);
+    PU_RUN_TEST(test_not_collide);
 }
 
 int main()
