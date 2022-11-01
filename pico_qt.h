65b609363770c2ec4ea13b9a9798f3c3c6f5160c
James McLean <james@wolfspider.ca>
2022-11-01 14:52:52 -0400

    Sketch out functions

--- pico_qt.h
+++ pico_qt.h
@@ -17,22 +17,45 @@ typedef struct
 
 typedef uint32_t qt_id_t;
 
-typedef union
+typedef enum
 {
-    qt_id_t id;
-    void*   ptr;
+    QT_VALUE_ID,
+    QT_VALUE_PTR
+} qt_value_type_t;
+
+typedef struct
+{
+    qt_value_type_t type;
+
+    union
+    {
+        qt_id_t id;
+        void*   ptr;
+    } value;
 } qt_value_t;
 
 typedef struct qt_t qt_t;
 typedef struct qt_node_t qt_node_t;
 typedef struct qt_array_t qt_array_t;
 
+//qt_value_t qt_make_id(qt_id_t id)
+//qt_value_t qt_make_ptr(void* ptr)
+//qt_value_equal(qt_value_t v1, qt_value_t v2);
+
 #endif // PICO_QT_H
 
 #ifdef PICO_QT_IMPLEMENTATION
 
+// PICO_QT_MAX_DEPTH
+// PICO_QT_MALLOC
+// PICO_QT FREE
+// PICO_QT_REALLOC
+// PICO_QT_MIN_ARRAY_SIZE
+
 struct qt_array_t
 {
+    int size;
+    int capacity;
     qt_value_t* array;
 };
 
@@ -43,15 +66,36 @@ struct qt_t
 
 struct qt_node_t
 {
-    int depth;
+    int        depth;
     qt_rect_t  bounds;
     qt_rect_t  regions[4];
     qt_node_t* children[4];
     qt_array_t items;
 };
 
+// qt_new
+// qt_free
+// qt_reset
+// qt_insert(qt, bounds, value)
+// qt_remove(qt, value)
+// qt_query(qt, area, array) //or
+
 // qt_rect_make
 // qt_rect_contains
 // qt_rect_overlaps
+
+// qt_array_new
+// qt_array_free
+// qt_array_size
+// qt_array_values
+
+// qt_array_push
+// qt_array_concat
+
+// qt_node_new
+// qt_node_free
+// qt_node_insert
+// qt_node_remove
+// qt_node_query
 
 #endif // PICO_QT_IMPLEMENTATION
