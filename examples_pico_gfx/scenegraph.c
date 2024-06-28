5bf4fd9c9dbfaa24f2391f5df64b85dbd458230c
James McLean <james@wolfspider.ca>
2024-06-27 21:21:31 -0400

    pico_gfx: removed flip-y from shader

--- examples_pico_gfx/scenegraph.c
+++ examples_pico_gfx/scenegraph.c
@@ -388,9 +388,9 @@ int main(int argc, char *argv[])
     pg_set_projection(ctx, (pg_mat4_t)
     {
         2.0f / w,  0.0f,     0.0f, 0.0f,
-        0.0f,      2.0f / h, 0.0f, 0.0f,
+        0.0f,      -2.0f / h, 0.0f, 0.0f,
         0.0f,      0.0f,     0.0f, 0.0f,
-       -1.0f,     -1.0f,     0.0f, 1.0f
+        -1.0f,     1.0f,     0.0f, 1.0f
     });
 
     pg_pipeline_t* pip = pg_create_pipeline(ctx, default_shader, &(pg_pipeline_opts_t)
