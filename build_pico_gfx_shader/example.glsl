32e247fbec4f294429e98d997a494e48d2ad2b9a
James McLean <james@wolfspider.ca>
2024-07-01 16:58:25 -0400

    pico_gfx: quad example runs, problem with shader uniforms

--- build_pico_gfx_shader/example.glsl
+++ build_pico_gfx_shader/example.glsl
@@ -4,16 +4,17 @@ layout(location = 0) in vec3 a_pos;
 layout(location = 1) in vec4 a_color;
 layout(location = 2) in vec2 a_uv;
 
-layout(binding = 0) uniform vs_block {
+/*layout(binding = 0) uniform vs_block {
     mat4 u_proj;
     mat4 u_tr;
-};
+};*/
 
 out vec4 color;
 out vec2 uv;
 
 void main() {
-    gl_Position = u_proj * u_tr * vec4(a_pos, 1.0);
+    //gl_Position = u_proj * u_tr * vec4(a_pos, 1.0);
+    gl_Position = vec4(a_pos, 1.0);
     color = a_color;
     uv = a_uv;
 }
