7216061c1fab2afd3af2ac9edf8171164ae9ad1a
James McLean <james@wolfspider.ca>
2026-05-30 22:38:25 -0400

    pico_ecs: updated rogue demo

--- examples_pico_ecs/rogue/factories.c
+++ examples_pico_ecs/rogue/factories.c
@@ -7,20 +7,23 @@ ecs_entity_t make_player(game_t* game, int x, int y)
     ecs_t* ecs = game->ecs;
     ecs_entity_t entity = ecs_create(ecs);
 
-    pos_t* pos = ecs_add(ecs, entity, POS_COMP, NULL);
+    ecs_add(ecs, entity, POS_COMP);
+    pos_t* pos = ecs_get(ecs, entity, POS_COMP);
     pos->x = x;
     pos->y = y;
 
-    drawable_t* drawable = ecs_add(ecs, entity, DRAWABLE_COMP, NULL);
+    ecs_add(ecs, entity, DRAWABLE_COMP);
+    drawable_t* drawable = ecs_get(ecs, entity, DRAWABLE_COMP);
     drawable->symbol = '@';
     drawable->visible = true;
 
-    stats_t* stats = ecs_add(ecs, entity, STATS_COMP, NULL);
+    ecs_add(ecs, entity, STATS_COMP);
+    stats_t* stats = ecs_get(ecs, entity, STATS_COMP);
     stats->health  = 10;
     stats->attack  = 5;
     stats->defense = 3;
 
-    ecs_add(ecs, entity, PLAYER_COMP, NULL);
+    ecs_add(ecs, entity, PLAYER_COMP);
 
     return entity;
 }
@@ -30,20 +33,24 @@ ecs_entity_t make_snake(game_t* game, int x, int y)
     ecs_t* ecs = game->ecs;
     ecs_entity_t entity = ecs_create(ecs);
 
-    pos_t* pos = ecs_add(ecs, entity, POS_COMP, NULL);
+    ecs_add(ecs, entity, POS_COMP);
+    pos_t* pos = ecs_get(ecs, entity, POS_COMP);
     pos->x = x;
     pos->y = y;
 
-    stats_t* stats = ecs_add(ecs, entity, STATS_COMP, NULL);
+    ecs_add(ecs, entity, STATS_COMP);
+    stats_t* stats = ecs_get(ecs, entity, STATS_COMP);
     stats->health  = 5;
     stats->attack  = 4;
     stats->defense = 1;
 
-    drawable_t* drawable = ecs_add(ecs, entity, DRAWABLE_COMP, NULL);
+    ecs_add(ecs, entity, DRAWABLE_COMP);
+    drawable_t* drawable = ecs_get(ecs, entity, DRAWABLE_COMP);
     drawable->symbol = 'S';
     drawable->visible = true;
 
-    monster_t* monster = ecs_add(ecs, entity, MONSTER_COMP, NULL);
+    ecs_add(ecs, entity, MONSTER_COMP);
+    monster_t* monster = ecs_get(ecs, entity, MONSTER_COMP);
     monster->state = MONSTER_WANDER;
     monster->type = MONSTER_SNAKE;
 
@@ -55,20 +62,24 @@ ecs_entity_t make_goblin(game_t* game, int x, int y)
     ecs_t* ecs = game->ecs;
     ecs_entity_t entity = ecs_create(ecs);
 
-    pos_t* pos = ecs_add(ecs, entity, POS_COMP, NULL);
+    ecs_add(ecs, entity, POS_COMP);
+    pos_t* pos = ecs_get(ecs, entity, POS_COMP);
     pos->x = x;
     pos->y = y;
 
-    stats_t* stats = ecs_add(ecs, entity, STATS_COMP, NULL);
+    ecs_add(ecs, entity, STATS_COMP);
+    stats_t* stats = ecs_get(ecs, entity, STATS_COMP);
     stats->health  = 10;
     stats->attack  = 6;
     stats->defense = 2;
 
-    drawable_t* drawable = ecs_add(ecs, entity, DRAWABLE_COMP, NULL);
+    ecs_add(ecs, entity, DRAWABLE_COMP);
+    drawable_t* drawable = ecs_get(ecs, entity, DRAWABLE_COMP);
     drawable->visible = true;
     drawable->symbol = 'G';
 
-    monster_t* monster = ecs_add(ecs, entity, MONSTER_COMP, NULL);
+    ecs_add(ecs, entity, MONSTER_COMP);
+    monster_t* monster = ecs_get(ecs, entity, MONSTER_COMP);
     monster->state = MONSTER_WANDER;
     monster->type = MONSTER_GOBLIN;
 
@@ -80,20 +91,24 @@ ecs_entity_t make_orc(game_t* game, int x, int y)
     ecs_t* ecs = game->ecs;
     ecs_entity_t entity = ecs_create(ecs);
 
-    pos_t* pos = ecs_add(ecs, entity, POS_COMP, NULL);
+    ecs_add(ecs, entity, POS_COMP);
+    pos_t* pos = ecs_get(ecs, entity, POS_COMP);
     pos->x = x;
     pos->y = y;
 
-    stats_t* stats = ecs_add(ecs, entity, STATS_COMP, NULL);
+    ecs_add(ecs, entity, STATS_COMP);
+    stats_t* stats = ecs_get(ecs, entity, STATS_COMP);
     stats->health  = 12;
     stats->attack  = 7;
     stats->defense = 3;
 
-    drawable_t* drawable = ecs_add(ecs, entity, DRAWABLE_COMP, NULL);
+    ecs_add(ecs, entity, DRAWABLE_COMP);
+    drawable_t* drawable = ecs_get(ecs, entity, DRAWABLE_COMP);
     drawable->visible = true;
     drawable->symbol = 'O';
 
-    monster_t* monster = ecs_add(ecs, entity, MONSTER_COMP, NULL);
+    ecs_add(ecs, entity, MONSTER_COMP);
+    monster_t* monster = ecs_get(ecs, entity, MONSTER_COMP);
     monster->state = MONSTER_WANDER;
     monster->type = MONSTER_ORC;
 
@@ -105,20 +120,24 @@ ecs_entity_t make_king(game_t* game, int x, int y)
     ecs_t* ecs = game->ecs;
     ecs_entity_t entity = ecs_create(ecs);
 
-    pos_t* pos = ecs_add(ecs, entity, POS_COMP, NULL);
+    ecs_add(ecs, entity, POS_COMP);
+    pos_t* pos = ecs_get(ecs, entity, POS_COMP);
     pos->x = x;
     pos->y = y;
 
-    stats_t* stats = ecs_add(ecs, entity, STATS_COMP, NULL);
+    ecs_add(ecs, entity, STATS_COMP);
+    stats_t* stats = ecs_get(ecs, entity, STATS_COMP);
     stats->health  = 20;
     stats->attack  = 10;
     stats->defense = 5;
 
-    drawable_t* drawable = ecs_add(ecs, entity, DRAWABLE_COMP, NULL);
+    ecs_add(ecs, entity, DRAWABLE_COMP);
+    drawable_t* drawable = ecs_get(ecs, entity, DRAWABLE_COMP);
     drawable->visible = true;
     drawable->symbol = 'K';
 
-    monster_t* monster = ecs_add(ecs, entity, MONSTER_COMP, NULL);
+    ecs_add(ecs, entity, MONSTER_COMP);
+    monster_t* monster = ecs_get(ecs, entity, MONSTER_COMP);
     monster->state = MONSTER_WANDER;
     monster->type = MONSTER_ORC;
 
@@ -130,16 +149,19 @@ ecs_entity_t make_chest(game_t* game, int x, int y)
     ecs_t* ecs = game->ecs;
     ecs_entity_t entity = ecs_create(ecs);
 
-    pos_t* pos = ecs_add(ecs, entity, POS_COMP, NULL);
+    ecs_add(ecs, entity, POS_COMP);
+    pos_t* pos = ecs_get(ecs, entity, POS_COMP);
     pos->x = x;
     pos->y = y;
 
-    chest_comp_t* chest = ecs_add(ecs, entity, CHEST_COMP, NULL);
+    ecs_add(ecs, entity, CHEST_COMP);
+    chest_comp_t* chest = ecs_get(ecs, entity, CHEST_COMP);
     chest->attack = random_int(0, 5);
     chest->health = random_int(0, 5);
     chest->defense = random_int(0, 5);
 
-    drawable_t* drawable = ecs_add(ecs, entity, DRAWABLE_COMP, NULL);
+    ecs_add(ecs, entity, DRAWABLE_COMP);
+    drawable_t* drawable = ecs_get(ecs, entity, DRAWABLE_COMP);
     drawable->symbol = 'C';
     drawable->visible = false;
 
